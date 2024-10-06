9f426ed72aeb307d5e774ee978a3317f2f7007ad
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-07 04:15:07 +0600

    name added to navbar

--- src/Layout/shared/NavBar.jsx
+++ src/Layout/shared/NavBar.jsx
@@ -34,7 +34,7 @@ const NavBar = () => {
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Mystic Aura</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -52,7 +52,7 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Login</a>
                 </div>
             </div>
         </div>
