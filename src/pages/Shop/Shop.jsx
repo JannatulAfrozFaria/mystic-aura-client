64a1328d331e19e09180844d14a015c6ef5c4c28
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:11:46 +0600

    heading updated for shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -5,7 +5,20 @@ const Shop = () => {
         <div>
             <div className="relative">
                 <img className="h-[50vh] w-full" src="https://i.postimg.cc/XvdM0q9C/shop-b2.png" alt="shop_banner" />
-                <h1>Discover Your Scent with Mystic Aura – Where Elegance Meets Fragrance</h1>
+                <h1 className="absolute top-12 p-12 md:p-24 uppercase text-2xl md:text-7xl text-basic">
+                        Discover Your Scent with Mystic Aura –{' '} {/* First word of the title */}
+                                    <span className='brandSpan'>
+                                        <Typewriter
+                                            words={['Where Elegance Meets Fragrance']} // Rest of the words joined as a string
+                                            loop={true}
+                                            cursor
+                                            cursorStyle='_'
+                                            typeSpeed={70}
+                                            deleteSpeed={50}
+                                            delaySpeed={3000}
+                                        />
+                                    </span>
+                                </h1>
             </div>
         </div>
     );
