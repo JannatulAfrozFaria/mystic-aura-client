25ce75ec1d795c0fce4ad82c6e9b5c90f396d1be
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 02:24:37 +0600

    button added for shopping

--- src/pages/Home/HomeComponents/Discount.jsx
+++ src/pages/Home/HomeComponents/Discount.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Discount = () => {
@@ -10,8 +11,13 @@ const Discount = () => {
                 <div className="bg-[#FBCFE8] p-8 grid grid-cols-1">
                         <div className="bg-white rounded-full w-20">
                             <p className="uppercase" >Discount</p>
-                            <h1>Winter 2024</h1>
+                            <h1 className="text-3xl">Winter 2024</h1>
                             <p className="uppercase priceTag" >Sale <span>50%</span> </p>
+                        </div>
+                        <div>
+                            <button>
+                                <Link to={'/shop'}></Link>
+                            </button>
                         </div>
                 </div>
             </div>
