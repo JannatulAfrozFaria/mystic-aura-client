76d77a5b661c0c51565b6130c3693f3ba64112ef
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-07 07:48:38 +0600

    interval and transition time set for banner

--- src/pages/Home/HomeComponents/Banner.jsx
+++ src/pages/Home/HomeComponents/Banner.jsx
@@ -11,7 +11,9 @@ const Banner = () => {
         },
         {
             id: 2,
-            image: "https://i.postimg.cc/0jf3W8tS/banner-2.avif",
+            // image: "https://i.postimg.cc/0jf3W8tS/banner-2.avif",
+            // image: "https://i.postimg.cc/wjKvc0FN/banner2-p.avif",
+            image: "https://i.postimg.cc/YC3vrQfm/b2cr.png",
             title: "Dunhill Icon"
         },
         {
@@ -26,65 +28,48 @@ const Banner = () => {
         },
         {
             id: 5,
-            image: "https://i.postimg.cc/T2VX5xcf/banner-5.avif",
+            // image: "https://i.postimg.cc/T2VX5xcf/banner-5.avif",
+            // image: "https://i.postimg.cc/jSzj8PkF/banner2.avif",
+            image: "https://i.postimg.cc/xCk83hrK/b5cr.png",
             title: "Yves Saint Laurent"
         },
         {
             id: 6,
-            image: "https://i.postimg.cc/vHZS8wqP/banner-7.jpg",
+            // image: "https://i.postimg.cc/vHZS8wqP/banner-7.jpg",
+            // image: "https://i.postimg.cc/brDy1ttL/b6cr.jpg",
+            // image: "https://i.postimg.cc/T2VX5xcf/banner-5.avif",
+            image: "https://i.postimg.cc/263rfZ2m/banner-b-7.jpg",
             title: "Atkinsons Oud"
         },
     ];
 
     return (
         <div >
-            <Carousel>
+            <Carousel swipeable={true} interval={4000} autoPlay={true} infiniteLoop={true} transitionTime={3000}>
                 {
-                    Banners.map((banner) =>
-                        <div key={banner.id} className="h-[90vh]">
-                            {/* <img src="https://i.ibb.co/YhznL0P/banner-6.jpg" /> */}
-                            <img className="relative" src="https://i.postimg.cc/SsbvmX38/banner-b-1.jpg" />
-                            <h1 className="absolute top-12 p-12 md:p-24 uppercase text-2xl md:text-7xl  text-basic">
-                                {banner.title[0]}{' '}
-                                <span className='brandSpan'>
-                                    <Typewriter
-                                        words={[`${banner.title[1]}`]}
-                                        loop={true}
-                                        cursor
-                                        cursorStyle='_'
-                                        typeSpeed={70}
-                                        deleteSpeed={50}
-                                        delaySpeed={3000}
-                                    />
-                                </span>
-                            </h1>
-                        </div>
-                    )
+                    Banners.map((banner) => {
+                        const [firstWord, ...restWords] = banner.title.split(' '); // Split the title into first word and rest of the words
+                        return (
+                            <div  key={banner.id} className="h-[90vh]">
+                                <img className="relative" src={banner.image} alt={banner.title} />
+                                <h1 className="absolute top-12 p-12 md:p-24 uppercase text-2xl md:text-7xl text-basic">
+                                    {firstWord}{' '} {/* First word of the title */}
+                                    <span className='brandSpan'>
+                                        <Typewriter
+                                            words={[restWords.join(' ')]} // Rest of the words joined as a string
+                                            loop={true}
+                                            cursor
+                                            cursorStyle='_'
+                                            typeSpeed={70}
+                                            deleteSpeed={50}
+                                            delaySpeed={3000}
+                                        />
+                                    </span>
+                                </h1>
+                            </div>
+                        );
+                    })
                 }
-                {/* <div className="h-[90vh]">
-                    <img className="relative" src="https://i.postimg.cc/SsbvmX38/banner-b-1.jpg" />
-                    <p className="legend bannerText">Trussardi Donna</p>
-                </div>
-                <div className="h-[90vh]">
-                    <img src="https://i.postimg.cc/0jf3W8tS/banner-2.avif" />
-                    <p className="legend">Dunhill Icon</p>
-                </div>
-                <div className="h-[90vh]">
-                    <img src="https://i.postimg.cc/zvFmswfV/banner-3.avif" />
-                    <p className="legend">Gucci Bloom</p>
-                </div>
-                <div className="h-[90vh]">
-                    <img src="https://i.postimg.cc/kg4rHbPg/banner-4.avif" />
-                    <p className="legend">Ralph Lauren </p>
-                </div>
-                <div className="h-[90vh]">
-                    <img src="https://i.postimg.cc/T2VX5xcf/banner-5.avif" />
-                    <p className="legend">Yves Saint Laurent</p>
-                </div>
-                <div className="h-[90vh]">
-                    <img src="https://i.postimg.cc/vHZS8wqP/banner-7.jpg" />
-                    <p className="legend">Atkinsons Oud</p>
-                </div> */}
             </Carousel>
         </div>
     );
