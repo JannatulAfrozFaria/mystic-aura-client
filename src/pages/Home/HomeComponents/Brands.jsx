70fb849ef33c70960fecd39e5c5020c8cf197972
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 04:15:41 +0600

    title added to Brands

--- src/pages/Home/HomeComponents/Brands.jsx
+++ src/pages/Home/HomeComponents/Brands.jsx
@@ -1,9 +1,10 @@
+import Title from "../../../UtilityComponents/Title";
 
 
 const Brands = () => {
     return (
         <div>
-            
+            <Title heading={'Client Reviews'} image={'https://i.postimg.cc/rmnRSHW9/logo-p.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} description={""}></Title>
         </div>
     );
 };
