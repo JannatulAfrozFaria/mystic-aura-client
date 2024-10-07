e16b6ea17b908f99d96c0a1d1dadf85a3cf11485
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-07 21:54:19 +0600

    slider added to featured section

--- src/pages/Home/HomeComponents/FeaturedPerfumes.jsx
+++ src/pages/Home/HomeComponents/FeaturedPerfumes.jsx
@@ -11,6 +11,24 @@ const FeaturedPerfumes = () => {
     return (
         <div className="">
             <Title heading={'Featured Product'} image={'https://i.postimg.cc/rmnRSHW9/logo-p.png'} description={'Discover our featured perfumes – a curated collection of luxury fragrances that captivate the senses. From timeless classics to modern scents, each perfume embodies elegance and allure, designed to leave a lasting impression'} ></Title>
+            <div>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    {perfumes.map((perfume)=>
+                    <SwiperSlide key={perfume.id}>
+                        <img className=" w-full h-[40vh] shadow-2xl" src={perfume.image} alt="perfume-photo" />
+                    </SwiperSlide>
+                    )}
+                </Swiper>
+            </div>
         </div>
     );
 };
