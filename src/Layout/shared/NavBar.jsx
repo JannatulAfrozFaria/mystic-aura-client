b0551a140464d95feea58eddce7575ba0c91e07f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-07 06:08:43 +0600

    company name added

--- src/Layout/shared/NavBar.jsx
+++ src/Layout/shared/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-transparent">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -33,14 +33,14 @@ const NavBar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] roboto font-semibold mt-3 w-52 p-2 shadow">
                             {navItems}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl protest">Mystic Aura</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul className="menu menu-horizontal px-1 roboto font-semibold">
                         {navItems}
                     </ul>
                 </div>
--- src/pages/Home/HomeComponents/Banner.jsx
+++ src/pages/Home/HomeComponents/Banner.jsx
@@ -3,30 +3,32 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
     return (
-        <div>
+        <div >
             <Carousel>
-                <div>
-                    <img src="https://i.ibb.co/YhznL0P/banner-6.jpg" />
+                <div className="h-[80vh]">
+                    {/* <img src="https://i.ibb.co/YhznL0P/banner-6.jpg" /> */}
+                    <img src="https://i.postimg.cc/SsbvmX38/banner-b-1.jpg" />
                     <p className="legend">Legend 1</p>
                 </div>
-                <div>
+                <div className="h-[80vh]">
                     <img src="https://i.postimg.cc/0jf3W8tS/banner-2.avif" />
                     <p className="legend">Legend 2</p>
                 </div>
-                <div>
+                <div className="h-[80vh]">
                     <img src="https://i.postimg.cc/zvFmswfV/banner-3.avif" />
                     <p className="legend">Legend 3</p>
                 </div>
-                <div>
+                <div className="h-[80vh]">
                     <img src="https://i.postimg.cc/kg4rHbPg/banner-4.avif" />
                     <p className="legend">Legend 4</p>
                 </div>
-                <div>
+                <div className="h-[80vh]">
                     <img src="https://i.postimg.cc/T2VX5xcf/banner-5.avif" />
                     <p className="legend">Legend 5</p>
                 </div>
-                <div>
-                    <img src="https://i.postimg.cc/76Sjz10m/banner-b-8.avif" />
+                <div className="h-[80vh]">
+                    {/* <img src="https://i.postimg.cc/76Sjz10m/banner-b-8.avif" /> */}
+                    <img src="https://i.postimg.cc/vHZS8wqP/banner-7.jpg" />
                     <p className="legend">Legend 6</p>
                 </div>
             </Carousel>
