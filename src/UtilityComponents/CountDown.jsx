ca77d5dc9e7a0c9df50a261056134ebe3965ea08
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 02:58:37 +0600

    minute and second formula added

--- src/UtilityComponents/CountDown.jsx
+++ src/UtilityComponents/CountDown.jsx
@@ -3,6 +3,9 @@ import { useEffect, useRef, useState } from "react";
 const formatTime = (time) =>{
     let minutes = Math.floor(time/60)
     let seconds = Math.floor(time - minutes*60)
+    if(minutes <= 10) minutes = '0' + minutes;
+    if(seconds <= 10) seconds = '0' + seconds;
+    return minutes + ':' + seconds
 }
 const CountDown =({seconds}) => {
     const [countdown,setCountdown] = useState(seconds)
