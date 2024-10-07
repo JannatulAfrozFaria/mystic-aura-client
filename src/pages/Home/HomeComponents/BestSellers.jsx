71136b1d537f2fea5521a0d176a49207725329d2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:33:02 +0600

    image width and height customised for best seller cards

--- src/pages/Home/HomeComponents/BestSellers.jsx
+++ src/pages/Home/HomeComponents/BestSellers.jsx
@@ -10,7 +10,7 @@ const BestSellers = () => {
                 {perfumes.slice(40,48).map((perfume)=>
                 <div key={perfume.id} className='grid grid-cols-5 gap-4'>
                     <div className='col-span-2'>
-                        <img src={perfume.image} alt="perfume_image" />
+                        <img className='h-[30vh]' src={perfume.image} alt="perfume_image" />
                     </div>
                     <div className='col-span-3'>
                         <h1> {perfume.name} </h1>
