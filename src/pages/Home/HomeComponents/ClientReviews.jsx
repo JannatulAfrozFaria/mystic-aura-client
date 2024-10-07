61b41e82b0bba85542eb85648e5e48b33934c63b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 04:14:59 +0600

    title logo updated

---
+++ src/pages/Home/HomeComponents/ClientReviews.jsx
@@ -0,0 +1,12 @@
+import Title from "../../../UtilityComponents/Title";
+
+
+const ClientReviews = () => {
+    return (
+        <div>
+             <Title heading={'Client Reviews'} image={'https://i.postimg.cc/rmnRSHW9/logo-p.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} description={""}></Title>
+        </div>
+    );
+};
+
+export default ClientReviews;
