549fd256a79fe366926705daa603f1998de93c4c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 04:17:25 +0600

    image class modified

--- src/pages/Home/HomeComponents/ClientReviews.jsx
+++ src/pages/Home/HomeComponents/ClientReviews.jsx
@@ -4,7 +4,7 @@ import Title from "../../../UtilityComponents/Title";
 const ClientReviews = () => {
     return (
         <div>
-             <Title heading={'Client Reviews'} image={'https://i.postimg.cc/rmnRSHW9/logo-p.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} description={""}></Title>
+             <Title heading={'Client Reviews'} image={'https://i.postimg.cc/rmnRSHW9/logo-p.png'} description={""}></Title>
         </div>
     );
 };
