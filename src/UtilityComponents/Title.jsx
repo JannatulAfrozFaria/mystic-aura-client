3d2936ecea6e33f285525f4c56ffb154b90dc4c8
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-07 08:04:09 +0600

    description and image assigned in Title

--- src/UtilityComponents/Title.jsx
+++ src/UtilityComponents/Title.jsx
@@ -4,6 +4,12 @@ const Title = ({heading, description,image}) => {
     return (
         <div>
             <h1>{heading} </h1>
+            <div>
+                <hr />
+                <img src={image} alt="" />
+                <hr />
+            </div>
+            <p>{description} </p>
         </div>
     );
 };
