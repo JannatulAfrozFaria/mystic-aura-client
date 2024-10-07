087ecf7bcb53b07dbe43adc3da4234b003ff21b7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 02:34:08 +0600

    gap added to text grid

--- src/pages/Home/HomeComponents/Discount.jsx
+++ src/pages/Home/HomeComponents/Discount.jsx
@@ -8,11 +8,15 @@ const Discount = () => {
                 <div>
                     <img src="" alt="" />
                 </div>
-                <div className="bg-[#FBCFE8] p-8 grid grid-cols-1">
+                <div className="bg-[#FBCFE8] p-8 grid grid-cols-1 gap-4">
                         <div className="bg-white rounded-full w-20">
                             <p className="uppercase" >Discount</p>
                             <h1 className="text-3xl text-dark protest ">Winter 2024</h1>
                             <p className="uppercase priceTag" >Sale <span>50%</span> </p>
+                        </div>
+                        {/* COUNT----DOWN */}
+                        <div>
+
                         </div>
                         <div>
                             <button className="text-dark roboto uppercase border-2 border-b-black">
