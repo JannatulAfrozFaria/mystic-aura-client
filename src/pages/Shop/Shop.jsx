4902f8cbfdbd8523b937c3b1fb79b61dc00857bf
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:08:35 +0600

    shoppinf page banner created

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -3,7 +3,10 @@
 const Shop = () => {
     return (
         <div>
-            
+            <div className="relative">
+                <img src="https://i.postimg.cc/XvdM0q9C/shop-b2.png" alt="shop_banner" />
+                <h1>Discover Your Scent with Mystic Aura – Where Elegance Meets Fragrance</h1>
+            </div>
         </div>
     );
 };
