53efd9e6dc2b873c6f1949d417af67912e8a1553
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 02:18:14 +0600

    grid layout created for discount

--- src/pages/Home/HomeComponents/Discount.jsx
+++ src/pages/Home/HomeComponents/Discount.jsx
@@ -3,7 +3,16 @@
 const Discount = () => {
     return (
         <div>
-            <div className="flex flex-col-reverse md:flex-row"></div>
+            <div className="flex flex-col-reverse md:flex-row">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div className="bg-[#FBCFE8] p-8 grid grid-cols-1">
+                        <div className="">
+                            <p className="uppercase" >Discount</p>
+                        </div>
+                </div>
+            </div>
         </div>
     );
 };
