981705af97daf92bec885f5c3496e0a101e25f71
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 04:12:41 +0600

    title imported to testimonials

--- src/pages/Home/HomeComponents/Testimonial.jsx
+++ src/pages/Home/HomeComponents/Testimonial.jsx
@@ -1,9 +1,10 @@
+import Title from "../../../UtilityComponents/Title";
 
 
 const Testimonial = () => {
     return (
         <div>
-            
+             <Title heading={'Client Reviews'} image={'https://i.postimg.cc/0y5NHLYT/mystic-aura-logo.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} description={""}></Title>
         </div>
     );
 };
