edbb1501c66b7ff5846c76f212eace92f3b8ffd8
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:14:17 +0600

    categories imported to heading

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,25 +1,27 @@
 
 import { Typewriter } from 'react-simple-typewriter';
+import Categories from '../Home/HomeComponents/Categories';
 const Shop = () => {
     return (
         <div>
             <div className="relative">
                 <img className="h-[50vh] w-full" src="https://i.postimg.cc/XvdM0q9C/shop-b2.png" alt="shop_banner" />
                 <h1 className="absolute top-12 p-12 md:p-24 uppercase text-2xl md:text-7xl text-basic">
-                        Discover Your Scent with Mystic Aura –{' '}
-                                    <span className='brandSpan'>
-                                        <Typewriter
-                                            words={['Where Elegance Meets Fragrance']} 
-                                            loop={true}
-                                            cursor
-                                            cursorStyle='_'
-                                            typeSpeed={70}
-                                            deleteSpeed={50}
-                                            delaySpeed={3000}
-                                        />
-                                    </span>
-                                </h1>
+                    Discover Your Scent with Mystic Aura –{' '}
+                    <span className='brandSpan'>
+                        <Typewriter
+                            words={['Where Elegance Meets Fragrance']}
+                            loop={true}
+                            cursor
+                            cursorStyle='_'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={3000}
+                        />
+                    </span>
+                </h1>
             </div>
+            <Categories></Categories>
         </div>
     );
 };
