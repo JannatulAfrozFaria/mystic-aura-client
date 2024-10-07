9966c335bc2f03e51bf416bf2fe2e620f9a9680e
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 04:20:10 +0600

    margin added to brands

--- src/pages/Home/HomeComponents/Brands.jsx
+++ src/pages/Home/HomeComponents/Brands.jsx
@@ -3,7 +3,7 @@ import Title from "../../../UtilityComponents/Title";
 
 const Brands = () => {
     return (
-        <div>
+        <div className="my-12 md:my-24">
             <Title heading={'Client Reviews'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} ></Title>
         </div>
     );
