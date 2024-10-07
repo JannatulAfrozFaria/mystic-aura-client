48569f8295379c7d46388d28aff8a4e2bcc6b950
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:27:47 +0600

    opacity set up for shopping banner

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -4,7 +4,7 @@ import Categories from '../Home/HomeComponents/Categories';
 const Shop = () => {
     return (
         <div>
-            <div className="relative">
+            <div className="relative bg-black opacity-30">
                 <img className="h-[50vh] w-full" src="https://i.postimg.cc/XvdM0q9C/shop-b2.png" alt="shop_banner" />
                 <h1 className="absolute top-12 p-12 md:p-24 uppercase text-2xl md:text-7xl text-basic">
                     Discover Your Scent with Mystic Aura –{' '}
