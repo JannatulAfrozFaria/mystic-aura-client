0df6367d20a7d33142e35b1a6489216d2d042333
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-08 00:31:17 +0600

    margin set up to best seller section

--- src/pages/Home/HomeComponents/BestSellers.jsx
+++ src/pages/Home/HomeComponents/BestSellers.jsx
@@ -4,7 +4,7 @@ import PefumeData from '/src/perfumes.json'
 const BestSellers = () => {
     const perfumes = PefumeData.perfumes;
     return (
-        <div>
+        <div className='my-12'>
              <Title heading={'Best Sellers'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'} description={"Discover our Best Sellers – a collection of top-rated fragrances that have captivated customers worldwide. From timeless classics to modern scents, these perfumes are loved for their unique blends, long-lasting essence, and luxurious appeal. Explore the finest fragrances, perfect for any occasion, and find your signature scent among the best of the best."}></Title>
             <div className="grid grid-cols-2 gap-6">
                 {perfumes.slice(40,48).map((perfume)=>
