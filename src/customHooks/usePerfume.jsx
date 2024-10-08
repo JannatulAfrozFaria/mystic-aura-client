1cde32011f74829767b792a98548d5d719724637
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 01:50:33 +0600

    configuration for perfumes.json imported

--- src/customHooks/usePerfume.jsx
+++ src/customHooks/usePerfume.jsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react";
 
 const usePerfume = () => {
+    const [perfumeCollection,setPerfumeCollection] = useState([])
+    useEffect(()=>{
+        fetch('/perfumeCollection.json')
+        .then(res => res.json())
+        .then(data => setPerfumeCollection(data))
+    },[perfumeCollection])
     return (
         <div>
             
