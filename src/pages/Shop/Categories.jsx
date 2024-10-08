0c4db63e4131dcf3d25c39a2ea034a1c5e5482ec
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 04:13:38 +0600

    tab lay out imported from daisy ui

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -24,6 +24,8 @@ const Categories = () => {
     return (
         <div className="my-12">
             <Title heading={'Categories'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'}></Title>
+            {/* TAB------- */}
+            {/* CATEGORIES */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 w-4/5 mx-auto">
                 {woody.map((perfume)=>
                     <div  key={perfume.id} className="flex flex-col">
@@ -34,13 +36,13 @@ const Categories = () => {
                             <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'> {perfume.name} </h1>
                             <div className="mt-1 md:mt-3 text-sm md:text-lg ">
                                 <p><span>Brand:</span> {perfume.brandName} </p>
-                                {/* <p><span>Origin:</span> {perfume.origin} </p> */}
+                                <p><span>Origin:</span> {perfume.origin} </p>
                                 {/* <p><span>Made for:</span> {perfume.userGroup} </p>
                                 <p><span>Category:</span> {perfume.category} </p> */}
-                                <p><span>Stock:</span> {perfume.quantity} Units </p>
-                                <p><span className=''>Price:</span>  <span className='bestPrice'> $ {perfume.price}</span></p>
-                                <button className="btn categoryButton">
-                                   <Link to={'/shop'}> Buy </Link>
+                                {/* <p><span>Stock:</span> {perfume.quantity} Units </p> */}
+                                <p><span className=''>Price:</span>  <span className='categoryPrice text-xl tracking-wider font-light'> $ {perfume.price}</span></p>
+                                <button className="btn categoryButton w-1/3 mx-auto mt-4 md:mt-2 text-2xl font-light">
+                                   <Link to={'/shop'}> Order </Link>
                                 </button>
                             </div>
                         </div>
