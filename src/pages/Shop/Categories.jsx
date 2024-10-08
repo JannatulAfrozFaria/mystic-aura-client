623f5f31ac5d82ca2a1578b1c864b6f13eaf002a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 02:02:03 +0600

    category check created for Men

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -9,7 +9,9 @@ import { Pagination } from 'swiper/modules';
 import { useEffect, useState } from "react";
 import usePerfume from "../../customHooks/usePerfume";
 const Categories = () => {
-    const {perfumes,loading}= usePerfume()
+    const {perfumes,loading}= usePerfume();
+    const men = perfumes.filter(item => item.category.toLowerCase() === 'men');
+
     // useEffect(()=>{
     //     fetch('/perfumeCollection.json')
     //     .then(res => res.json())
@@ -20,7 +22,7 @@ const Categories = () => {
         <div className="my-12">
             <Title heading={'Categories'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'}></Title>
             <div className="grid grid-cols-3 gap-4">
-                {perfumeCollection.map((perfume)=>
+                {men.map((perfume)=>
                     <div key={perfume.id}>
                         <img className=" w-full h-[40vh] shadow-2xl" src={perfume.image} alt="perfume-photo" />
                     </div >
