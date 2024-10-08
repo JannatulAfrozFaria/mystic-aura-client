c6e56f25d9c73b9d6402f4ca135f469cbe04059b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 05:24:44 +0600

    card imported to Tab component

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -45,11 +45,7 @@ const Categories = () => {
                         <Tab>Chypre</Tab>
                     </TabList>
                     <TabPanel className={''}>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 mt-10 md:mt-16 mb-12 md:mb-32 w-5/6 mx-auto md:w-full">
-                            {woody.map((perfume) =>
-                                <CategoryCard key={perfume.id} item={perfume} />
-                            )}
-                        </div>
+                        
                     </TabPanel>
                     <TabPanel>
 
