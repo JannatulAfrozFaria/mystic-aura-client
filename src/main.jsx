4b5bd6f88ead06adc1a9939d8f514a3b9b37af99
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 01:17:55 +0600

    helmet imported to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HelmetProvider>
     <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>,
 )
