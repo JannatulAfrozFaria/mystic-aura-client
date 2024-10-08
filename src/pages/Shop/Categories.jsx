992795123c02c0c2b06188a0fe9f3270092ce67c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 01:42:24 +0600

    reviews json created

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -6,39 +6,25 @@ import 'swiper/css/pagination';
 // import './styles.css';
 import PefumeData from '/src/perfumes.json'
 import { Pagination } from 'swiper/modules';
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 const Categories = () => {
-    // const [perfumes,setPerfumes] = useState([])
-    // useEffect(()=>{
-    //     fetch('../../../perfumes.json')
-    //     .then(res => res.json())
-    //     .then(data => setPerfumes(data))
-    // },[])
-    const perfumes = PefumeData.perfumes;
+    const [perfumeCollection,setPerfumeCollection] = useState([])
+    useEffect(()=>{
+        fetch('/perfumeCollection.json')
+        .then(res => res.json())
+        .then(data => setPerfumeCollection(data))
+    },[perfumeCollection])
+    // const perfumes = PefumeData.perfumes;
     return (
-        <div className="mb-12">
-            
-            {/* <Title heading={'Categories'} image={'https://i.postimg.cc/0y5NHLYT/mystic-aura-logo.png'}></Title> */}
+        <div className="my-12">
             <Title heading={'Categories'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'}></Title>
-            {/* <div>
-                <Swiper
-                    slidesPerView={4}
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[Pagination]}
-                    className="mySwiper"
-                >
-                    {perfumes.map((perfume)=>
-                    <SwiperSlide key={perfume.id}>
+            <div className="grid grid-cols-3 gap-4">
+                {perfumeCollection.map((perfume)=>
+                    <div key={perfume.id}>
                         <img className=" w-full h-[40vh] shadow-2xl" src={perfume.image} alt="perfume-photo" />
-                    </SwiperSlide>
+                    </div >
                     )}
-                </Swiper>
-            </div> */}
-            
+            </div>
         </div>
     );
 };
