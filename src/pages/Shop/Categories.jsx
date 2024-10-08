e5e493286a841943ac798da74fd2214e6e206f40
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 03:32:29 +0600

    categories made responsive for mobile device

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Title from "../../UtilityComponents/Title";
 import usePerfume from "../../customHooks/usePerfume";
 const Categories = () => {
@@ -23,21 +24,24 @@ const Categories = () => {
     return (
         <div className="my-12">
             <Title heading={'Categories'} image={'https://i.postimg.cc/6p4mvJsB/logo-p2.png'} imageClass={'border-2 rounded-full border-[#d282afbc]'}></Title>
-            <div className="grid grid-cols-3 gap-4 md:gap-8 w-4/5 mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 w-4/5 mx-auto">
                 {woody.map((perfume)=>
                     <div  key={perfume.id} className="flex flex-col">
                         <div className="flex justify-center items-end  h-[30vh]">
-                            <img className=" w-1/2 h-[180px] categoryImageBorder " src={perfume.image} alt="perfume-photo" />
+                            <img className=" w-1/2 h-[150px] md:h-[180px] categoryImageBorder " src={perfume.image} alt="perfume-photo" />
                         </div>
-                        <div className='col-span-3 p-4 md:pt-16 md:pb-0 md:px-12 h-full text-gray-500 roboto shadow-2xl categoryTextBorder cardTextProperty text-center'>
-                            <h1 className='text-2xl md:text-4xl protest pName'> {perfume.name} </h1>
+                        <div className='col-span-3 pt-16 pb-0 px-6 md:px-12 h-full text-gray-500 roboto shadow-2xl categoryTextBorder cardTextProperty text-center'>
+                            <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'> {perfume.name} </h1>
                             <div className="mt-1 md:mt-3 text-sm md:text-lg ">
                                 <p><span>Brand:</span> {perfume.brandName} </p>
                                 {/* <p><span>Origin:</span> {perfume.origin} </p> */}
                                 {/* <p><span>Made for:</span> {perfume.userGroup} </p>
                                 <p><span>Category:</span> {perfume.category} </p> */}
                                 <p><span>Stock:</span> {perfume.quantity} Units </p>
-                                <p><span className=''>Price:</span>  <span className='bestPrice'> $ {perfume.price}</span>  </p>
+                                <p><span className=''>Price:</span>  <span className='bestPrice'> $ {perfume.price}</span></p>
+                                <button>
+                                   <Link> Buy </Link>
+                                </button>
                             </div>
                         </div>
 
