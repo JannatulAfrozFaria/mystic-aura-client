1dcfd732594b74e1c6a5ab738c179545846474ab
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 01:53:47 +0600

    variable created for loading

--- src/customHooks/usePerfume.jsx
+++ src/customHooks/usePerfume.jsx
@@ -1,17 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const usePerfume = () => {
-    const [perfumeCollection,setPerfumeCollection] = useState([])
+    const [perfumeCollection,setPerfumeCollection] = useState([]);
+    const [loading,setLoading] = useState(true)
     useEffect(()=>{
         fetch('/perfumeCollection.json')
         .then(res => res.json())
         .then(data => setPerfumeCollection(data))
     },[perfumeCollection])
-    return (
-        <div>
-            
-        </div>
-    );
+    return {perfumeCollection}
 };
 
 export default usePerfume;
