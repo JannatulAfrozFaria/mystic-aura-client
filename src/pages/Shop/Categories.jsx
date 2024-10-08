62a1d21568e7987f5ef17de22626f557bde50626
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-09 04:50:41 +0600

    item passed from category component

--- src/pages/Shop/Categories.jsx
+++ src/pages/Shop/Categories.jsx
@@ -4,6 +4,7 @@ import usePerfume from "../../customHooks/usePerfume";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { useState } from "react";
+import CategoryCard from "./CategoryCard";
 const Categories = () => {
     const [tabIndex, setTabIndex] = useState(0);
     const { perfumes, loading } = usePerfume();
@@ -46,25 +47,26 @@ const Categories = () => {
                     <TabPanel>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 w-4/5 mx-auto">
                             {woody.map((perfume) =>
-                                <div key={perfume.id} className="flex flex-col">
-                                    <div className="flex justify-center items-end  h-[30vh]">
-                                        <img className=" w-1/2 h-[150px] md:h-[180px] categoryImageBorder " src={perfume.image} alt="perfume-photo" />
-                                    </div>
-                                    <div className='col-span-3 pt-16 pb-0 px-6 md:px-12 h-full text-gray-500 roboto shadow-2xl categoryTextBorder cardTextProperty text-center'>
-                                        <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'> {perfume.name} </h1>
-                                        <div className="mt-1 md:mt-3 text-sm md:text-lg ">
-                                            <p><span>Brand:</span> {perfume.brandName} </p>
-                                            <p><span>Origin:</span> {perfume.origin} </p>
-                                            {/* <p><span>Made for:</span> {perfume.userGroup} </p>
-                                <p><span>Category:</span> {perfume.category} </p> */}
-                                            {/* <p><span>Stock:</span> {perfume.quantity} Units </p> */}
-                                            <p><span className=''>Price:</span>  <span className='categoryPrice text-xl tracking-wider font-light'> $ {perfume.price}</span></p>
-                                            <button className="btn categoryButton w-1/3 mx-auto mt-4 md:mt-2 text-2xl font-light">
-                                                <Link to={'/shop'}> Order </Link>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div >
+                            <CategoryCard key={perfume.id} items={perfume} ></CategoryCard>
+                                // <div key={perfume.id} className="flex flex-col">
+                                //     <div className="flex justify-center items-end  h-[30vh]">
+                                //         <img className=" w-1/2 h-[150px] md:h-[180px] categoryImageBorder " src={perfume.image} alt="perfume-photo" />
+                                //     </div>
+                                //     <div className='col-span-3 pt-16 pb-0 px-6 md:px-12 h-full text-gray-500 roboto shadow-2xl categoryTextBorder cardTextProperty text-center'>
+                                //         <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'> {perfume.name} </h1>
+                                //         <div className="mt-1 md:mt-3 text-sm md:text-lg ">
+                                //             <p><span>Brand:</span> {perfume.brandName} </p>
+                                //             <p><span>Origin:</span> {perfume.origin} </p>
+                                //             {/* <p><span>Made for:</span> {perfume.userGroup} </p>
+                                // <p><span>Category:</span> {perfume.category} </p> */}
+                                //             {/* <p><span>Stock:</span> {perfume.quantity} Units </p> */}
+                                //             <p><span className=''>Price:</span>  <span className='categoryPrice text-xl tracking-wider font-light'> $ {perfume.price}</span></p>
+                                //             <button className="btn categoryButton w-1/3 mx-auto mt-4 md:mt-2 text-2xl font-light">
+                                //                 <Link to={'/shop'}> Order </Link>
+                                //             </button>
+                                //         </div>
+                                //     </div>
+                                // </div >
                             )}
                         </div>
                     </TabPanel>
--- src/pages/Shop/CategoryCard.jsx
+++ src/pages/Shop/CategoryCard.jsx
@@ -10,12 +10,12 @@ const CategoryCard = ({perfume}) => {
                     <img className=" w-1/2 h-[150px] md:h-[180px] categoryImageBorder " src={image} alt="perfume-photo" />
                 </div>
                 <div className='col-span-3 pt-16 pb-0 px-6 md:px-12 h-full text-gray-500 roboto shadow-2xl categoryTextBorder cardTextProperty text-center'>
-                    <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'> {name} </h1>
+                    <h1 className='text-2xl md:text-4xl protest pName w-3/4 md:w-full mx-auto'>{name}</h1>
                     <div className="mt-1 md:mt-3 text-sm md:text-lg ">
                         <p><span>Brand:</span> {brandName} </p>
                         <p><span>Origin:</span> {origin} </p>
                         {/* <p><span>Made for:</span> {perfume.userGroup} </p>
-                                <p><span>Category:</span> {perfume.category} </p> */}
+                            <p><span>Category:</span> {perfume.category} </p> */}
                         {/* <p><span>Stock:</span> {perfume.quantity} Units </p> */}
                         <p><span className=''>Price:</span>  <span className='categoryPrice text-xl tracking-wider font-light'> $ {price}</span></p>
                         <button className="btn categoryButton w-1/3 mx-auto mt-4 md:mt-2 text-2xl font-light">
