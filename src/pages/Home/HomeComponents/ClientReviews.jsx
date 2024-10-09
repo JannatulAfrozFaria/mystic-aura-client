cfc4c2a06f8846c8e4a83caabf192f03f5724a74
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-10 04:38:54 +0600

    pagination modules imported from swiper js

--- src/pages/Home/HomeComponents/ClientReviews.jsx
+++ src/pages/Home/HomeComponents/ClientReviews.jsx
@@ -13,16 +13,14 @@ const ClientReviews = () => {
     //     .then(res => res.json())
     //     .then(data => setReviews(data))
     // },[reviews])
-    
+
     const [reviews,setReviews] = useState([])
     useEffect(()=>{
         fetch('http://localhost:5000/reviews')
         .then(res => res.json())
         .then(data => setReviews(data))
     },[reviews])
-
-   
-
+    
     // const customers = [
     //     {
     //         id: 1,
--- src/pages/Shop/CategoryTab.jsx
+++ src/pages/Shop/CategoryTab.jsx
@@ -1,7 +1,16 @@
 import CategoryCard from "./CategoryCard";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 const CategoryTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-16 mt-10 md:mt-16 mb-12 md:mb-32 w-5/6 mx-auto md:w-full">
@@ -9,6 +18,13 @@ const CategoryTab = ({ items }) => {
                     <CategoryCard key={perfume.id} item={perfume} />
                 )}
             </div>
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+            </Swiper>
         </div>
     );
 };
