b43a48b6c4498569ef2a921d2b2aafc4eec45b51
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-11 04:43:34 +0600

    text error solved

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,63 +1,3 @@
-// import { AiFillGoogleCircle } from "react-icons/ai";
-// import { FaGithub } from "react-icons/fa6";
-// const Login = () => {
-//     return (
-//         <div>
-//             <div className="w-3/4 mx-auto min-h-screen">
-//                 <div className=" text-basic flex h-full justify-center items-center">
-//                     <div className=" card-body w-4/5 mx-auto text-center bg-black h-full">
-//                         <h1 className="text-5xl font-bold">Login to <span className="protest" >Mystic <span>Aura !</span></span></h1>
-//                         <p className="py-6">
-//                             Don't have an account?
-//                         </p>
-//                         <div className="form-control mt-6">
-//                             <button className="btn btn-outline tracking-wider font-light text-xl btn-basic">Sign Up</button>
-//                         </div>
-//                     </div>
-//                     {/* FORM SECTION--------- */}
-//                     <div className=" card-body bg-[#FBCFE8] text-black w-full  shadow-2xl">
-//                     <div className="flex justify-between ">
-//                             <div>
-//                                 <h1 className="text-xl font-medium">Login</h1>
-//                             </div>
-//                             <div className="flex gap-2 text-3xl">
-//                                 <div>
-//                                     <AiFillGoogleCircle />
-//                                 </div>
-//                                 <div>
-//                                     <FaGithub />
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <form className="">
-//                             <div className="form-control">
-//                                 <label className="label">
-//                                     <span className="label-text">Email</span>
-//                                 </label>
-//                                 <input type="email" placeholder="email" className="input input-bordered" required />
-//                             </div>
-//                             <div className="form-control">
-//                                 <label className="label">
-//                                     <span className="label-text">Password</span>
-//                                 </label>
-//                                 <input type="password" placeholder="password" className="input input-bordered" required />
-//                                 <label className="label">
-//                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-//                                 </label>
-//                             </div>
-//                             <div className="form-control mt-6">
-//                                 <button className="btn categoryButton tracking-wider font-light text-xl">Login</button>
-//                             </div>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Login;
-
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa6";
 
@@ -77,7 +17,7 @@ const Login = () => {
                     <div className=" text-center bg-black p-8 md:p-0 flex flex-col justify-center h-full flex-grow text-basic ">
                         <h1 className="text-5xl font-bold  ">Login to <br /> </h1>
                         <h1 className=" text-5xl protest mt-4 text-[#c97ca9]">Mystic Aura!</h1>
-                        <p className="py-6 ">Don't have an account?</p>
+                        <p className="py-6 ">Don&apos;t have an account?</p>
                         <div className="form-control w-1/2 md:w-1/3 mx-auto ">
                             <button className="btn text-xl text-white btn-basic">Sign Up</button>
                         </div>
