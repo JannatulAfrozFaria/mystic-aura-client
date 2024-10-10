9a5be16cc17ea250a5891766c8684197405afba3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-11 02:05:47 +0600

    noheader conditional set for login page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./shared/Footer";
 import NavBar from "./shared/NavBar";
 
 
 const Main = () => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname.includes('/login')
     return (
         <div>
-            <NavBar></NavBar>
+            {/* <NavBar></NavBar> */}
+            { noHeaderFooter || <NavBar></NavBar>}
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
