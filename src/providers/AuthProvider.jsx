f591a5a92735a9c79020e480fe3e18b6a75fe611
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-12 02:41:19 +0600

    auth imported in AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,9 @@
 import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
     const [loading,setLoading] = useState(true);
