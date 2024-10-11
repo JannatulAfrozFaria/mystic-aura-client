a44c37c5e7e98d45d9de930e738d463d5ce716e7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-12 03:05:27 +0600

    login and logOut function added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -8,10 +8,19 @@ const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
     const [loading,setLoading] = useState(true);
     const createUser = (email,password)=>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password)
     }
+    const login = (email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth)
+    }
     useEffect(()=>{
-       const  unsubscribe  =  onAuthStateChanged(auth,currentUser=>{
+       const unsubscribe  =  onAuthStateChanged(auth,currentUser=>{
             setUser(currentUser);
             console.log('current-user', currentUser);
             setLoading(false);
@@ -23,7 +32,10 @@ const AuthProvider = ({children}) => {
     const authInfo ={
         user,
         loading,
-        createUser
+        createUser,
+        login,
+        logOut
+
     }
     return (
         <AuthContext.Provider value ={authInfo} >
