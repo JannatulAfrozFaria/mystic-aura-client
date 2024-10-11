73d22826a87921950d613c4256e06bf490d38428
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-12 03:20:49 +0600

    signUp and Login Added to no header condition

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,7 +5,7 @@ import NavBar from "./shared/NavBar";
 
 const Main = () => {
     const location = useLocation();
-    const noHeaderFooter = location.pathname.includes('/login')
+    const noHeaderFooter = location.pathname.includes('/login') || location.pathname.includes('/signUp')
     return (
         <div>
             {/* <NavBar></NavBar> */}
