6c6c21d3dea48c2d492f9d99bf8150675e0526b5
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-13 17:43:17 +0600

    naviation to login added to private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 
 const PrivateRoute = ({children}) => {
     const {user} = useContext(AuthContext);
-    return (
-        <div>
-            
-        </div>
-    );
+    if(user){
+        return children;
+    }
+    return <Navigate to={'/login'} ></Navigate>
 };
 
 export default PrivateRoute;
