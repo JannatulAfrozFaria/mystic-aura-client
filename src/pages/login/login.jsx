c1526a7862aa9e786a17096c1cfecf1325273d74
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-21 01:40:14 +0600

    relative absolute style added for positioning icon

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -96,9 +96,11 @@ const Login = () => {
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type={showPassword? "text" : "password"} 
-                                    name="password" placeholder="password" className="input input-bordered" required />
-                                    <span onClick={()=> setShowPassword(!showPassword)} > {showPassword ? <FaEyeSlash /> : <FaEye /> }  </span>
+                                    <div className="relative">
+                                        <input type={showPassword? "text" : "password"} 
+                                        name="password" placeholder="password" className="input input-bordered" required />
+                                        <span className="absolute" onClick={()=> setShowPassword(!showPassword)} > {showPassword ? <FaEyeSlash /> : <FaEye /> }  </span>
+                                        </div>
                                     {/* <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label> */}
