86cdf097f46781742c495e6d3b14c9075fcb9bdd
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-22 02:37:41 +0600

    outlet created for dashboard

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,13 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 
 const Dashboard = () => {
     return (
         <div className="flex">
-            <div className="w-">
+            <div className="w-64 min-h-full basicBg text-dark2">
                 <ul className="menu">
-                    <li><NavLink to={'/dashboard/cart'}>My Cart</NavLink></li>
+                    <li><NavLink to={'/dashboard/cart'}> My Cart </NavLink></li>
                 </ul>
+            </div>
+            <div>
+                <Outlet></Outlet>
             </div>
         </div>
     );
