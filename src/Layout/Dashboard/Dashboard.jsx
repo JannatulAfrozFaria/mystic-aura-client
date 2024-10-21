b3932b67dfae10b24103577e2e6a1461f00cb0fe
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-22 02:44:44 +0600

    height updated for dashboard

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { NavLink, Outlet } from "react-router-dom";
 const Dashboard = () => {
     return (
         <div className="flex">
-            <div className="w-64 min-h-full basicBg text-dark2">
+            <div className="w-64 h-full basicBg text-dark2">
                 <ul className="menu">
                     <li><NavLink to={'/dashboard/cart'}> My Cart </NavLink></li>
                 </ul>
