22d2d7d06ae969df4506acee1ca71b70ae8b22b4
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-05-21 02:56:22 +0600

    update delete function

--- src/Dashboard_Pages/Cart.jsx
+++ src/Dashboard_Pages/Cart.jsx
@@ -9,51 +9,72 @@ const Cart = () => {
     const {cart,refetch} = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     const axiosSecure = useAxiosSecure();
-    const handleDelete = (id) =>{
-        console.log(id);
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          })
-        //   .then((result) => {
-        //     if (result.isConfirmed) {
-        //       axiosSecure.delete(`/carts/${id}`)
-        //       .then(res=>{
-        //         console.log(res);
-        //         if(res.data.deletedCount>0){ 
-        //             Swal.fire({
-        //                 title: "Deleted!",
-        //                 text: "Your file has been deleted.",
-        //                 icon: "success"
-        //               });
-        //               refetch();
-        //         }
-        //       })
-        //     } 
-        //   });
-          .then((result)=>{
-            if(result.isConfirmed){
-                fetch(`https://localhost:5000/carts/${id}`,{
-                    method: 'DELETE'
-                })
-                .then(res=>res.json())
-                .then(data=>{
-                    if(data.deletedCount>0){
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: 'Item has been deleted',
-                            icon: "success"
-                        })
-                    }
-                })
-            }
-          })
-    }
+    // const handleDelete = (id) =>{
+    //     console.log(id);
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "You won't be able to revert this!",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, delete it!"
+    //       })
+    //       .then((result)=>{
+    //         if(result.isConfirmed){
+    //             fetch(`http://localhost:5000/carts/${id}`,{
+    //                 method: 'DELETE'
+    //             })
+    //             .then(res=>res.json())
+    //             .then(data=>{
+    //                 if(data.deletedCount>0){
+    //                     Swal.fire({
+    //                         title: "Deleted!",
+    //                         text: 'Item has been deleted',
+    //                         icon: "success"
+    //                     })
+    //                 }
+    //             })
+    //         }
+    //       })
+    // }
+    const handleDelete = (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    })
+    .then((result) => {
+        if (result.isConfirmed) {
+            fetch(`http://localhost:5000/carts/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: 'Item has been deleted',
+                        icon: "success"
+                    });
+                    refetch(); // Don't forget to call refetch
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                Swal.fire({
+                    title: "Error!",
+                    text: "Failed to delete item",
+                    icon: "error"
+                });
+            });
+        }
+    });
+};
     
     return (
         <div>
@@ -63,7 +84,7 @@ const Cart = () => {
             <div className="flex justify-evenly">
                 <h2 className="text-xl md:text-5xl">Items: {cart.length} </h2>
                 <h2 className="text-xl md:text-5xl">Total Price: <span className="dashBrand">$ {totalPrice}</span> </h2>
-                <button className="btn payButton tracking-widest w-full md:w-[200px] text-xl font-thin">Pay</button>
+                <button className="btn payButton tracking-widest  text-xl font-thin">Pay</button>
             </div>
             <div>
                 <div className="overflow-x-auto roboto my-8">
