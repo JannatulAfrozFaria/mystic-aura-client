e1e11e040f1fb7b49166cd604a6cf03604307c59
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-05-30 02:15:48 +0600

    update carts style for small screen

--- src/Dashboard_Pages/Cart.jsx
+++ src/Dashboard_Pages/Cart.jsx
@@ -8,35 +8,7 @@ const Cart = () => {
   const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   const axiosSecure = useAxiosSecure();
-  // const handleDelete = (id) =>{
-  //     console.log(id);
-  //     Swal.fire({
-  //         title: "Are you sure?",
-  //         text: "You won't be able to revert this!",
-  //         icon: "warning",
-  //         showCancelButton: true,
-  //         confirmButtonColor: "#3085d6",
-  //         cancelButtonColor: "#d33",
-  //         confirmButtonText: "Yes, delete it!"
-  //       })
-  //       .then((result)=>{
-  //         if(result.isConfirmed){
-  //             fetch(`http://localhost:5000/carts/${id}`,{
-  //                 method: 'DELETE'
-  //             })
-  //             .then(res=>res.json())
-  //             .then(data=>{
-  //                 if(data.deletedCount>0){
-  //                     Swal.fire({
-  //                         title: "Deleted!",
-  //                         text: 'Item has been deleted',
-  //                         icon: "success"
-  //                     })
-  //                 }
-  //             })
-  //         }
-  //       })
-  // }
+
   const handleDelete = (id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -53,7 +25,7 @@ const Cart = () => {
           .then((res) => {
             console.log(res);
             if (res.data.deletedCount > 0) {
-              refetch(); // Refresh cart data
+              refetch();
               Swal.fire({
                 title: "Deleted!",
                 text: "Item has been removed from your cart.",
@@ -78,9 +50,9 @@ const Cart = () => {
       <Helmet>
         <title>Mystic Aura | Cart</title>
       </Helmet>
-      <div className="flex justify-evenly">
-        <h2 className="text-xl md:text-5xl">Items: {cart.length} </h2>
-        <h2 className="text-xl md:text-5xl">
+      <div className="flex gap-1 md:gap-0 flex-col md:flex-row justify-center md:justify-evenly w-2/3 md:w-full mx-auto">
+        <h2 className="text-xl md:text-5xl text-center  ">Items: {cart.length} </h2>
+        <h2 className="text-xl md:text-5xl text-center">
           Total Price: <span className="dashBrand">$ {totalPrice}</span>{" "}
         </h2>
         <button className="btn payButton tracking-widest  text-xl font-thin">
@@ -93,37 +65,35 @@ const Cart = () => {
             {/* head */}
             <thead className="text-lg md:text-xl">
               <tr>
-                <th className="hidden md:block">#</th>
-                {/* <th className="hidden md:block">Image</th> */}
-                <th>Name</th>
-                <th>Price</th>
-                <th className="hidden md:block">Brand</th>
-                <th></th>
+                <th className="hidden md:table-cell align-middle">#</th>
+                <th className="hidden md:table-cell align-middle">Image</th>
+                <th className="align-middle">Name</th>
+                <th className="align-middle">Price</th>
+                <th className="hidden md:table-cell align-middle">Brand</th>
+                <th className="align-middle">Action</th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
               {cart.map((item, index) => (
                 <tr key={item._id}>
-                  <th className="hidden md:block">
-                    {index + 1}
-                  </th>
-                  {/* <td className="hidden md:block">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img src={item.image} alt="perfume-icon" />
+                  <th className="hidden md:table-cell align-middle">{index + 1}</th>
+                  <td className="hidden md:table-cell align-middle">
+                    <div className="avatar flex justify-start">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={item.image} alt={item.name} />
                       </div>
                     </div>
-                  </td> */}
-                  <td>
-                    <div className="flex items-center">
-                      {/* <div className="avatar">
-                                            <div className="mask mask-squircle h-12 w-12">
-                                                <img
-                                                    src={item.image}
-                                                    alt="perfume-icon" />
-                                            </div>
-                                        </div> */}
+                  </td>
+                  <td className="align-middle">
+                    <div className="flex items-center gap-3">
+                      {/* <div className="md:hidden">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-8 h-8">
+                            <img src={item.image} alt={item.name} />
+                          </div>
+                        </div>
+                      </div> */}
+                      {/* NAME--- */}
                       <div>
                         <div className="font-bold">{item.name}</div>
                         <div className="text-sm opacity-50">
@@ -132,28 +102,29 @@ const Cart = () => {
                       </div>
                     </div>
                   </td>
-                  <td>$ {item.price} </td>
-                  <td className="hidden md:block">
+                  <td className="align-middle">$ {item.price}</td>
+                  <td className="hidden md:table-cell align-middle">
                     <div className="font-bold">{item.brandName}</div>
                   </td>
-                  <th>
+                  <td className="align-middle">
                     <button
                       onClick={() => handleDelete(item._id)}
-                      className=" btn-xs md:btn-lg dashBrandSpan"
+                      className="btn-xs md:btn-lg dashBrandSpan"
                     >
-                      <FaTrash />{" "}
+                      <FaTrash />
                     </button>
-                  </th>
+                  </td>
                 </tr>
               ))}
             </tbody>
             {/* foot */}
             <tfoot className="text-xl">
               <tr>
-                <th></th>
-                <th>Price Total</th>
-                <th>$ {totalPrice}</th>
-                <th> </th>
+                <th className="hidden md:table-cell"></th>
+                <th className="hidden md:table-cell"></th>
+                <th className="align-middle">Price Total</th>
+                <th className="align-middle">$ {totalPrice}</th>
+                <th className="hidden md:table-cell"></th>
                 <th></th>
               </tr>
             </tfoot>
