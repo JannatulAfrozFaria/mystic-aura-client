132a091b71db0bdc165a5e7087ce58953b539af9
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-06-04 02:42:48 +0600

    add console for error

--- src/customHooks/useAxiosSecure.jsx
+++ src/customHooks/useAxiosSecure.jsx
@@ -4,6 +4,7 @@ const axiosSecure = axios.create({
     baseURL:'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    //request interceptor to add authorization header to every secure 
     axiosSecure.interceptors.request.use(function(config){
         const token = localStorage.getItem('access-token')
         console.log('request stopped by interceptors', token )
@@ -13,6 +14,14 @@ const useAxiosSecure = () => {
     }, function(error){
         return Promise.reject(error);
     } 
+)
+//intercepts 401 and 403
+axiosSecure.interceptors.response.use(function(response){
+    return response;
+}, (error) =>{
+    console.log('status error in the interceptor', error )
+    return Promise.reject(error);
+}
 )
     return axiosSecure;
 };
