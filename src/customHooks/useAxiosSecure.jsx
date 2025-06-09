d93e2c29aa56c7ea7493fb9012d80b1a237b4eeb
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-06-09 19:10:42 +0600

    add comment in axios secure

--- src/customHooks/useAxiosSecure.jsx
+++ src/customHooks/useAxiosSecure.jsx
@@ -24,6 +24,7 @@ axiosSecure.interceptors.response.use(function(response){
 }, async(error) =>{
     const status = error.response.status;
     console.log('status error in the interceptor', status )
+    //for 401 and 403 logout the user and move the user to login page
     if(status === 401 || status === 403 ){
         await logOut();
         navigate('/login')
